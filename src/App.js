bb94e1852eb308201a83178fd33db40999835cdb
Priti Nakhare <pritinakhare@Pritis-MacBook-Pro.local>
2022-12-06 15:36:08 +0530

    Assignment 2 changes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const h1 = React.createElement(
+    "h1", 
+    { class:"headingclass" },
+    "This code is created by React"
+  );
+
+  const div = React.createElement(
+    "div", 
+    { className: "democlass", },
+    "Welcome to Namaste Rect BootCamp",
+    h1
+  );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(div);
