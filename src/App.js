4468f7bc732c2d5673f1363af353f06e568fa35e
Priti Nakhare <pritinakhare@Pritis-MacBook-Pro.local>
2022-12-06 17:21:10 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import avatar from '../assets/avatar.png';
 
-const h1 = React.createElement(
-    "h1", 
-    { class:"headingclass" },
-    "This code is created by React"
-  );
+const LogoComponent = () => <img className="logo" src="https://lwfiles.mycourse.app/635a5db526fa77c557bab00d-public/c4377b05799f40987f16cacd43da014d.png"></img>;
+const SearchComponent = () => <input type="text" placeholder="Search Team Member.."></input>;
+const UserAvatarComponent = () => <img className="avatar" src={avatar}></img>;
 
-  const div = React.createElement(
-    "div", 
-    { className: "democlass", },
-    "Welcome to Namaste Rect BootCamp",
-    h1
-  );
+const HeadingComponent = () => (
+  <div id="header" className="header" tabIndex="1">
+    <span><LogoComponent /></span>
+    <span className="search-form"><SearchComponent/></span>
+    <span><UserAvatarComponent/></span>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(div);
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
